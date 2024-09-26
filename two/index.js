92d5e77af9bf870dbc62778414a19e54862e0975
Jasur Mansurov <makej1318@gmail.com>
2024-09-26 18:04:17 +0500

    starting

---
+++ two/index.js
@@ -0,0 +1,9 @@
+// '', "", ``
+// 0, 30, 4e10
+// true false
+var isBirthday = 1;
+var Alex = 'Alex';
+var age = 40;
+if (isBirthday) {
+    console.log("name ".concat(Alex, ", age: ").concat(age));
+}
---
+++ two/index.ts
@@ -0,0 +1,13 @@
+
+
+// '', "", ``
+// 0, 30, 4e10
+// true false
+
+const isBirthday: boolean = true;
+const Alex: string = 'Alex';
+const age: number = 40;
+
+if (isBirthday) {
+    console.log(`name ${Alex}, age: ${age + 1}`)
+}
